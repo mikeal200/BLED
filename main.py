44fdc6805b929c9a5365d1eaa9388f6130b3886b
mikeal200 <willi6mc@mail.uc.edu>
2021-01-06 20:05:31 -0500

    led bulb shifts through 3 colors then turns on and off

---
+++ main.py
@@ -0,0 +1,17 @@
+import pexpect
+import time 
+
+bulb = '44:A6:E5:1E:0D:F7'
+
+#red, green, blue, off, on
+nums = ['545c1df148b649494641', '545cfc10a9a857a8a7a0', '545c20cc7574748b7b7c', '545cf71ba7a3a3a3a3a3', '545c28c4787d7c7c7c7c']
+
+gatt = pexpect.spawn('gatttool -I')
+
+gatt.sendline('connect ' + bulb)
+gatt.expect('Connection successful')
+
+while True :
+    for i in nums :
+        gatt.sendline('char-write-cmd 0x0025 ' + i)
+        time.sleep(.5)
