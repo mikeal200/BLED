892732abeb9091b10118b2b4865ac004af91645e
mikeal200 <willi6mc@mail.uc.edu>
2021-01-07 01:08:25 -0500

    color light blue added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 
 bulb = '44:A6:E5:1E:0D:F7'
 
-#red, green, blue, pink, purple, orange, yellow, lime, off, on
+#red, green, blue, pink, purple, orange, yellow, lime, light blue, off, on
 nums = ['545c1df148b649494641', 
     '545cfc10a9a857a8a7a0',
     '545c20cc7574748b7b7c', 
@@ -11,7 +11,8 @@ nums = ['545c1df148b649494641',
     '545cfc10a93baf57a7a0',
     '545c20cc758b2b717b7c',
     '545c45a910eec4001e19',
-    '545caf43fa2d04f9f4f3', 
+    '545caf43fa2d04f9f4f3',
+    '545cf71ba2a05c11acab', 
     '545cf71ba7a3a3a3a3a3', 
     '545c28c4787d7c7c7c7c']
 
@@ -24,6 +25,7 @@ while True :
     for i in nums :
         gatt.sendline('char-write-cmd 0x0025 ' + i)
         time.sleep(.5)
+
 
 
 
