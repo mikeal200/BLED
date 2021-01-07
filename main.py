1562b736e42d522f23a58eec0a4afcad1dadbd29
mikeal200 <willi6mc@mail.uc.edu>
2021-01-07 01:21:02 -0500

    colors.py was created to store colors and state constants

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import pexpect
 import time 
+import colors
 
 bulb = '44:A6:E5:1E:0D:F7'
 
 #red, green, blue, pink, purple, orange, yellow, lime, light blue, off, on
-nums = ['545c1df148b649494641', 
-    '545cfc10a9a857a8a7a0',
-    '545c20cc7574748b7b7c', 
-    '545c1df148b64aa64641', 
-    '545cfc10a93baf57a7a0',
-    '545c20cc758b2b717b7c',
-    '545c45a910eec4001e19',
-    '545caf43fa2d04f9f4f3',
-    '545cf71ba2a05c11acab', 
-    '545cf71ba7a3a3a3a3a3', 
-    '545c28c4787d7c7c7c7c']
+nums = [
+    RED,
+    GREEN,
+    BLUE,
+    PINK,
+    ORANGE,
+    YELLOW,
+    LIME,
+    LIGHT_BLUE
+]
 
 gatt = pexpect.spawn('gatttool -I')
 
