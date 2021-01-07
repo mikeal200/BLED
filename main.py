df500519e539d7874514e9b2794f7c1bb28ddaa6
mikeal200 <willi6mc@mail.uc.edu>
2021-01-07 01:02:27 -0500

    color lime added

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import time
 
 bulb = '44:A6:E5:1E:0D:F7'
 
-#red, green, blue, pink, purple, orange, yellow, off, on
+#red, green, blue, pink, purple, orange, yellow, lime, off, on
 nums = ['545c1df148b649494641', 
     '545cfc10a9a857a8a7a0',
     '545c20cc7574748b7b7c', 
     '545c1df148b64aa64641', 
     '545cfc10a93baf57a7a0',
     '545c20cc758b2b717b7c',
-    '545c45a910eec4001e19', 
+    '545c45a910eec4001e19',
+    '545caf43fa2d04f9f4f3', 
     '545cf71ba7a3a3a3a3a3', 
     '545c28c4787d7c7c7c7c']
 
