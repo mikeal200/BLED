3e1409235d5c3bc22f2e1ccbb31d2447c3198de5
mikeal200 <willi6mc@mail.uc.edu>
2021-01-07 00:54:45 -0500

    color purple added

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import time
 
 bulb = '44:A6:E5:1E:0D:F7'
 
-#red, green, blue, pink, off, on
-nums = ['545c1df148b649494641', '545cfc10a9a857a8a7a0', '545c20cc7574748b7b7c', '545c1df148b64aa64641','545cf71ba7a3a3a3a3a3', '545c28c4787d7c7c7c7c']
+#red, green, blue, pink, purple, off, on
+nums = ['545c1df148b649494641', 
+    '545cfc10a9a857a8a7a0',
+    '545c20cc7574748b7b7c', 
+    '545c1df148b64aa64641', 
+    '545cfc10a93baf57a7a0', 
+    '545cf71ba7a3a3a3a3a3', 
+    '545c28c4787d7c7c7c7c']
 
 gatt = pexpect.spawn('gatttool -I')
 
@@ -15,5 +21,6 @@ while True :
     for i in nums :
         gatt.sendline('char-write-cmd 0x0025 ' + i)
         time.sleep(.5)
+
 
 
